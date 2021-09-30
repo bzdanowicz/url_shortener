0078c81cfb0f36686df64070e4748b354d370857
Bartosz Zdanowicz <zdanowiczb@gmail.com>
2021-09-30 22:23:32 +0200

    Swagger/OpenAPI documentation added

--- go.mod
+++ go.mod
@@ -10,8 +10,10 @@ require (
 	github.com/go-redis/redis/v8 v8.11.3
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/spf13/viper v1.9.0
+	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2
+	github.com/swaggo/gin-swagger v1.3.2
+	github.com/swaggo/swag v1.7.3
 	github.com/ugorji/go v1.2.6 // indirect
 	go.mongodb.org/mongo-driver v1.7.2
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"github.com/bzdanowicz/url_shortener/utils"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+
+	_ "github.com/bzdanowicz/url_shortener/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeApplication(ctx context.Context) *storage.StorageManager {
@@ -23,6 +27,14 @@ func initializeApplication(ctx context.Context) *storage.StorageManager {
 	return &storage.StorageManager{Database: database, Cache: cache}
 }
 
+// HandleRedirect godoc
+// @Summary Link redirection.
+// @Description redirect to the original page
+// @Accept json
+// @Produce json
+// @Param url path string true "url"
+// @Success 302 "redirect to original page"
+// @Router /{url} [get]
 func HandleRedirect(c *gin.Context, storageManager *storage.StorageManager) {
 	url := c.Param("url")
 	originalUrl, err := storageManager.GetOriginalUrl(url)
@@ -37,6 +49,21 @@ type UrlCreationRequest struct {
 	OriginalUrl string `json:"original_url" binding:"required"`
 }
 
+type UrlCreationResponse struct {
+	Message     string `json:"message" binding:"required"`
+	OriginalUrl string `json:"original_url" binding:"required"`
+	NewUrl      string `json:"new_url" binding:"required"`
+}
+
+// CreateNewLink godoc
+// @Summary Creating new short link.
+// @Description shorten original link
+// @Accept json
+// @Produce json
+// @Param req body UrlCreationRequest true "OriginalUrl"
+// @Success 202 {object} UrlCreationResponse "Returns shorten url"
+// @Failure 400 {object} UrlCreationResponse "Failure response"
+// @Router /url [post]
 func CreateNewLink(c *gin.Context, storageManager *storage.StorageManager) {
 	request := &UrlCreationRequest{}
 	err := c.Bind(request)
@@ -53,7 +80,7 @@ func CreateNewLink(c *gin.Context, storageManager *storage.StorageManager) {
 
 	_, err = storageManager.GetOriginalUrl(encodedUrl)
 	if err == nil {
-		c.JSON(http.StatusAccepted, gin.H{"message": "Link already exists", "original_url": request.OriginalUrl, "new_url": encodedUrl})
+		c.JSON(http.StatusAccepted, &UrlCreationResponse{"Link already exists", request.OriginalUrl, encodedUrl})
 		return
 	}
 
@@ -62,7 +89,7 @@ func CreateNewLink(c *gin.Context, storageManager *storage.StorageManager) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusAccepted, gin.H{"message": "Created new link", "original_url": request.OriginalUrl, "new_url": url.Id})
+	c.JSON(http.StatusAccepted, &UrlCreationResponse{"Created new link", request.OriginalUrl, url.Id})
 }
 
 func createRouting(r *gin.Engine, storageManager *storage.StorageManager) {
@@ -79,6 +106,8 @@ func createRouting(r *gin.Engine, storageManager *storage.StorageManager) {
 	r.POST("/url", func(c *gin.Context) {
 		CreateNewLink(c, storageManager)
 	})
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
 
 func runRouter(storageManager *storage.StorageManager) {
